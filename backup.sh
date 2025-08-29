f35afe473e94affe33f7d91beec8d46e05ce1c4c
MichielDePaepe <michiel.de.paepe@gmail.com>
2025-08-29 10:51:03 +0200

    Backup op 2025-08-29 10:51:03

--- backup.sh
+++ backup.sh
@@ -5,7 +5,3 @@ pip freeze > requirements.txt
 git add -A
 git commit -m "Backup op $(date '+%Y-%m-%d %H:%M:%S')"
 git push
-
-
-echo "Backup voltooid"
-git remote add origin git@github.com:michieldepaepe/loxonewebserver.git
