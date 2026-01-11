fea8390f7a506603b7f6f684ccb80b614979db12
MichielDePaepe <michiel.de.paepe@gmail.com>
2026-01-11 23:18:21 +0100

    Backup op 2026-01-11 23:18:21

--- RadioAssetManagement/asgi.py
+++ RadioAssetManagement/asgi.py
@@ -1,24 +1,19 @@
-"""
-ASGI config for RadioAssetManagement project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
+# RadioAssetManagement/asgi.py
 
 import os
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+
 import roip.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RadioAssetManagement.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "RadioAssetManagement.settings")
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(URLRouter(roip.routing.websocket_urlpatterns)),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(roip.routing.websocket_urlpatterns)
+    ),
 })
