55bb8a8af0f6ee0d2775537470715c5d8fea565a
MichielDePaepe <michiel.de.paepe@gmail.com>
2026-01-18 07:46:26 +0100

    Backup op 2026-01-18 07:46:26

--- fireplan/tasks.py
+++ fireplan/tasks.py
@@ -1,6 +1,6 @@
 # fireplan/tasks.py
 from celery import shared_task
-from sync_inventory import sync_closed_inventories_portable_radio_teis
+from .sync_inventory import sync_closed_inventories_portable_radio_teis
 
 
 @shared_task
